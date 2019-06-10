f653118094bd78df3f61c76efd6cb60b8ed9b331
carlos tzompantzi <carlostz.ccd@gmail.com>
2019-06-10 14:10:27 -0500

    remove hard coded paths

--- init_project.sh
+++ init_project.sh
@@ -1,12 +1,13 @@
 if uname -a | grep --quiet Darwin; then
+  # before continue on mac install envsubst
   export $(grep -v '^#' .env | xargs -0)
 else
   export $(grep -v '^#' .env | xargs -d '\n')
 fi
 
-mkdir -p "$PWD/$PROJECT_DIR/" "$PWD/src/project/config"
+mkdir -p "$PWD/$PROJECT_DIR" "$PWD/$CONFIG_DIR"
 
-cat > "$PWD/src/project/config/pm2.json" <<EOF
+cat > "$PWD/$PM2_CONFIG_FILE" <<EOF
 {
   "name": "code",
   "script": "index.js"
@@ -14,7 +15,10 @@ cat > "$PWD/src/project/config/pm2.json" <<EOF
 
 EOF
 
+# create package.json in $PROJECT_DIR
 docker run --rm -ti -v "$PWD/$PROJECT_DIR":"$DOCKER_PROJECT_DIR" -w "$DOCKER_PROJECT_DIR" $NODE_IMAGE npm init
+# add express dependency in $PROJECT_DIR (add node_modules)
 docker run --rm -ti -v "$PWD/$PROJECT_DIR":"$DOCKER_PROJECT_DIR" -w "$DOCKER_PROJECT_DIR" $NODE_IMAGE npm i --save express
+# remove node_modules from $PROJECT_DIR
 rm -rf "$PWD/$PROJECT_DIR/node_modules"
 envsubst '$CONTAINER_DB_NAME' < docker-compose.yml_template > docker-compose.yml
