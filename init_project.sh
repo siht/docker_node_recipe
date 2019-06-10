3830ef4da79a1d4ae55797c84c94828c42c8e4e1
carlos tzompantzi <carlostz.ccd@gmail.com>
2019-06-10 13:45:39 -0500

    add support for mac

--- init_project.sh
+++ init_project.sh
@@ -1,4 +1,20 @@
-export $(grep -v '^#' .env | xargs -d '\n')
+if uname -a | grep --quiet Darwin; then
+  export $(grep -v '^#' .env | xargs -0)
+else
+  export $(grep -v '^#' .env | xargs -d '\n')
+fi
 
-docker run --rm -ti -v "$PWD/$PROJECT_DIR":"$DOCKER_PROJECT_DIR" -w "$DOCKER_PROJECT_DIR" $NODE_IMAGE npm install
+mkdir -p "$PWD/$PROJECT_DIR/" "$PWD/src/project/config"
+
+cat > "$PWD/src/project/config/pm2.json" <<EOF
+{
+  "name": "code",
+  "script": "index.js"
+}
+
+EOF
+
+docker run --rm -ti -v "$PWD/$PROJECT_DIR":"$DOCKER_PROJECT_DIR" -w "$DOCKER_PROJECT_DIR" $NODE_IMAGE npm init
+docker run --rm -ti -v "$PWD/$PROJECT_DIR":"$DOCKER_PROJECT_DIR" -w "$DOCKER_PROJECT_DIR" $NODE_IMAGE npm i --save express
+rm -rf "$PWD/$PROJECT_DIR/node_modules"
 envsubst '$CONTAINER_DB_NAME' < docker-compose.yml_template > docker-compose.yml
