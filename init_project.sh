023b9ef811111d478152b91ab7677fc93b4ed592
carlos <radix_10339@hotmail.com>
2019-06-10 10:49:03 -0500

    add a initiatio of docker compose

--- init_project.sh
+++ init_project.sh
@@ -1,3 +1,4 @@
 export $(grep -v '^#' .env | xargs -d '\n')
 
 docker run --rm -ti -v "$PWD/$PROJECT_DIR":"$DOCKER_PROJECT_DIR" -w "$DOCKER_PROJECT_DIR" $NODE_IMAGE npm install
+envsubst '$CONTAINER_DB_NAME' < docker-compose.yml_template > docker-compose.yml
